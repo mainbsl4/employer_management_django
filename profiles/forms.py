daa39d88d04193500e2bbf27dbf1591fc78a7ea9
mainbsl4 <mainbsl4@gmail.com>
2024-09-17 21:13:43 +0600

    main

--- profiles/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Profiles
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profiles
-        fields = ['bg_image', 'profile_image']
