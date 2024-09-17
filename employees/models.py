a2dc6532f840e55bb9c5a11aaa6611e4059d8490
mainbsl4 <mainbsl4@gmail.com>
2024-09-17 20:28:48 +0600

    main

--- employees/models.py
+++ employees/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Employees(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
-    phone_no = models.CharField(max_length=20)
+    phone_no = models.CharField(max_length=20, unique=True)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     designation = models.CharField(max_length=100)
     short_description = models.TextField()
